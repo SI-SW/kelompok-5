a1a69fd2eee4754215cac76974be2531c86b26be
Abdi Surya Perdana <abdisury4@gmail.com>
2022-10-23 20:31:03 +0700

    update sidenav todo button

--- src/examples/Sidenav/SidenavList.vue
+++ src/examples/Sidenav/SidenavList.vue
@@ -87,6 +87,17 @@
             <i class="ni ni-collection text-info text-sm opacity-10"></i>
           </template>
         </sidenav-item>
+        <li class="nav-item">
+        <sidenav-item
+          url="/auth/todo"
+          :class="getRoute() === 'todo' ? 'active' : ''"
+          :navText="this.$store.state.isRTL ? 'تسجيل الدخول' : 'To Do'"
+        >
+          <template v-slot:icon>
+            <i class="ni ni-single-copy-04 text-danger text-sm opacity-10"></i>
+          </template>
+        </sidenav-item>
+      </li>
       </li>
       <li class="nav-item">
         <a href="#" class="nav-link" @click="logout">
@@ -104,6 +115,7 @@
       textPrimary="Need Help?"
       textSecondary="Please check our docs"
     />
+    
   </div>
 </template>
 <script>
